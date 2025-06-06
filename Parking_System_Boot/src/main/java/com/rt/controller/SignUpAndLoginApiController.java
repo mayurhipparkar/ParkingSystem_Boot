7370a30e57ef2085abdcd90d1502e4279d528e8d
mayurhipparkar <mayurhipparkar048@gmail.com>
2025-06-06 08:41:08 +0530

    Default url declaired in controller

--- Parking_System_Boot/src/main/java/com/rt/controller/SignUpAndLoginApiController.java
+++ Parking_System_Boot/src/main/java/com/rt/controller/SignUpAndLoginApiController.java
@@ -10,9 +10,11 @@ import com.rt.serviceImplementation.SignUpImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
+@RequestMapping("/main")
 public class SignUpAndLoginApiController {
 	//sign-up logic start.
 	@Autowired
